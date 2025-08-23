fc75e7f8a05418367f9e3ffbb123c250ee1a51f0
Martin Metodiev <martin@mev.bg>
2025-08-23 13:32:39 +0300

    Move “system” to be first in possibleColorSchemeModes

--- src/runtime/composables/color-scheme.composable.ts
+++ src/runtime/composables/color-scheme.composable.ts
@@ -4,7 +4,7 @@ import { DEFAULT_COLOR_SCHEME } from '../defaults';
 
 export const colorSchemes = ['light', 'dark'] as const;
 // This is the list of possible color scheme modes, including the system mode
-export const possibleColorSchemeModes = [...colorSchemes, 'system'] as const;
+export const possibleColorSchemeModes = ['system', ...colorSchemes] as const;
 
 export type ColorSchemeKey = (typeof colorSchemes)[number];
 export type ColorSchemeMode = (typeof possibleColorSchemeModes)[number];
